85a5fa3a35243b80acdffd5ee649a234142e572f
Francis <brakay1136@gmail.com>
2025-06-08 09:24:02 -0700

    reset button  functionality done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const numberBtn = document.querySelectorAll(".num-btn");
 const operatorBtn = document.querySelectorAll(".operator");
 const keysContainer = document.querySelector(".number-keys");
 const screen = document.querySelector("#screen");
+const resetBtn = document.querySelector(".resetbtn");
 const equalBtn = document.querySelector(".equal-btn");
 
 const numOperators = ["+", "-", "/", "*"];
@@ -33,4 +34,8 @@ equalBtn.addEventListener("click", () => {
 
   let result = evalExp;
   screen.value = eval(result);
+});
+
+resetBtn.addEventListener("click", () => {
+  screen.value = 0;
 });
