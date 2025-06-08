85c676319dbffe0eea38bdb01718211109535c53
Francis <brakay1136@gmail.com>
2025-06-08 09:17:35 -0700

    created the calculation functionality

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const numberBtn = document.querySelectorAll(".num-btn");
+const operatorBtn = document.querySelectorAll(".operator");
 const keysContainer = document.querySelector(".number-keys");
 const screen = document.querySelector("#screen");
+const equalBtn = document.querySelector(".equal-btn");
+
+const numOperators = ["+", "-", "/", "*"];
+let displayExp = "";
+let evalExp = "";
 
 keysContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("num-btn")) {
-    numberBtn.forEach((key) => {
-      if (e.target === key) {
-        screen.value += key.textContent;
-      }
-    });
+  numberBtn.forEach((key) => {
+    if (e.target === key) {
+      displayExp += key.textContent;
+      evalExp += key.textContent;
+      screen.value = displayExp;
+    }
+  });
+
+  operatorBtn.forEach((operatorBtn, index) => {
+    if (e.target === operatorBtn) {
+      displayExp += operatorBtn.textContent;
+      evalExp += numOperators[index];
+      screen.value = displayExp;
+    }
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  if (screen.value === "") {
+    return;
   }
+
+  let result = evalExp;
+  screen.value = eval(result);
 });
