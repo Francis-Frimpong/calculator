6543952bbbb0c016dad257d8960215ef38c3d57f
Francis <brakay1136@gmail.com>
2025-09-30 08:29:03 -0700

    oop version done

--- oop-version.js
+++ oop-version.js
@@ -1,10 +1,11 @@
 class Calculator {
   constructor() {
+    //   properties
     this.numberBtn = document.querySelectorAll(".num-btn");
     this.operatorBtn = document.querySelectorAll(".operator");
     this.keysContainer = document.querySelector(".number-keys");
-    this.scrren = document.querySelector("#screen");
-    this.resetBtn = document.querySelector(".resetBtn");
+    this.screen = document.querySelector("#screen");
+    this.resetBtn = document.querySelector(".resetbtn");
     this.equalBtn = document.querySelector(".equal-btn");
     this.deleteBtn = document.querySelector(".delete-btn");
 
@@ -13,14 +14,13 @@ class Calculator {
     this.evalExp = "";
   }
 
-  // number button function
-
+  // number button method
   digitAndOperatorBtnEvent(e) {
     this.numberBtn.forEach((key) => {
       if (e.target === key) {
         this.displayExp += key.textContent;
         this.evalExp += key.textContent;
-        this.scrren.value = this.displayExp;
+        this.screen.value = this.displayExp;
       }
     });
 
@@ -28,25 +28,45 @@ class Calculator {
       if (e.target === operatorBtn) {
         this.displayExp += operatorBtn.textContent;
         this.evalExp += this.numOperators[index];
-        this.scrren.value = this.displayExp;
+        this.screen.value = this.displayExp;
       }
     });
   }
-  // equal button function
 
+  // equal button method
   equalBtnEvent() {
-    if (this.screeen.value === "") {
+    if (this.screen.value === "") {
       return;
     }
 
     let result = this.evalExp;
-    this.scrren.value = eval(result);
+    this.screen.value = eval(result);
   }
+
+  // reset button method
+  resetBtnEvent() {
+    let ledScreen = (this.screen.value = "");
+    this.displayExp = ledScreen;
+    this.evalExp = ledScreen;
+  }
+
+  // delete button method
+  deleteBtnEvent() {
+    if (this.displayExp !== "") {
+      this.displayExp = this.displayExp.slice(0, -1);
+      this.evalExp = this.evalExp.slice(0, -1);
+      this.screen.value = this.displayExp;
+    }
+  }
+
+  // handles all event listeners
   addEventListeners() {
     this.keysContainer.addEventListener("click", (e) =>
       this.digitAndOperatorBtnEvent(e)
     );
     this.equalBtn.addEventListener("click", () => this.equalBtnEvent());
+    this.resetBtn.addEventListener("click", () => this.resetBtnEvent());
+    this.deleteBtn.addEventListener("click", () => this.deleteBtnEvent());
   }
 }
 
